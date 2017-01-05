5e0d62f55840d09ccb26ed9fa75dc5bb8237e4b8
Athena Perez <athenaperez@me.com>
2017-01-05 10:51:33 -0600

    first round of exercise

--- myJavascript.js
+++ myJavascript.js
@@ -23,14 +23,27 @@ var randomUser = {
   ]
 }
 
-//handlebars step one: grap the html from the script tag
 
+
+//handlebars step one: grap the html from the script tag
+var entryHTML = $("#random-user").html();
 
 //handlebars step two: compile it into a template
-
+var entryTemplate = Handlebars.compile(entryHTML);
 
 //handlebars step three:render the HTML by passing the data to the template
-
+var entryOutput = entryTemplate(randomUser);
 
 //handlebars step four: put the complete HTML into the DOM
+$("#randomUserOutput").append(entryOutput);
+
+
+
+
+
+
+
+
+
+
 
